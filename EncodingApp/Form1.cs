2035d62c998ea7800fd17b9a6a25643a7e82eb8f
carakazov <carakazov@yandex.ru>
2021-05-11 22:27:49 +0300

    перестановки

--- EncodingApp/Form1.cs
+++ EncodingApp/Form1.cs
@@ -14,7 +14,7 @@ namespace EncodingApp
     public partial class Form1 : Form
     {
         private string alphabet;
-        private IEncoder encoder;
+        private ShuffleEncoder encoder;
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +22,7 @@ namespace EncodingApp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            encoder = new ShuffleEncoder(textBox1.Text);
             richTextBox2.Text = encoder.Encode(richTextBox1.Text);
         }
 
@@ -29,38 +30,7 @@ namespace EncodingApp
         {
             richTextBox1.Text = encoder.Decode(richTextBox2.Text);
         }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            DefineAlphabet();
-            encoder = new VigenereEncoder(textBox1.Text, alphabet);
-        }
         
-        private void DefineAlphabet()
-        {
-            if (radioButtonEnglish.Checked)
-            {
-                alphabet = Utils.LatinAlphabet;
-            }
-            else
-            {
-                alphabet = Utils.CyrillicAlphabet;
-            }
-        }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            encoder = new ShuffleEncoder(textBox1.Text);
-        }
-
-        private void checkBox3_CheckedChanged(object sender, EventArgs e)
-        {
-            encoder = new GammaEncoder();
-        }
-
-        private void checkBox4_CheckedChanged(object sender, EventArgs e)
-        {
-            encoder = new MatrixEncoder();
-        }
+        
     }
 }
