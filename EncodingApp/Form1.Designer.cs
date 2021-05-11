35f86069047fd43032b41e94566a18a410ba134d
carakazov <carakazov@yandex.ru>
2021-05-11 22:15:14 +0300

    Виженер

--- EncodingApp/Form1.Designer.cs
+++ EncodingApp/Form1.Designer.cs
@@ -38,19 +38,13 @@ namespace EncodingApp
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.radioButtonRussian = new System.Windows.Forms.RadioButton();
-            this.radioButtonEnglish = new System.Windows.Forms.RadioButton();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(664, 10);
+            this.label1.Location = new System.Drawing.Point(22, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 212);
+            this.label1.Size = new System.Drawing.Size(100, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ключ";
             // 
@@ -114,77 +108,11 @@ namespace EncodingApp
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.Location = new System.Drawing.Point(306, 12);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(153, 21);
-            this.checkBox1.TabIndex = 8;
-            this.checkBox1.Text = "Шифр Виженера";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // radioButtonRussian
-            // 
-            this.radioButtonRussian.Location = new System.Drawing.Point(277, 295);
-            this.radioButtonRussian.Name = "radioButtonRussian";
-            this.radioButtonRussian.Size = new System.Drawing.Size(156, 21);
-            this.radioButtonRussian.TabIndex = 9;
-            this.radioButtonRussian.TabStop = true;
-            this.radioButtonRussian.Text = "Русский";
-            this.radioButtonRussian.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonEnglish
-            // 
-            this.radioButtonEnglish.Location = new System.Drawing.Point(277, 322);
-            this.radioButtonEnglish.Name = "radioButtonEnglish";
-            this.radioButtonEnglish.Size = new System.Drawing.Size(125, 24);
-            this.radioButtonEnglish.TabIndex = 10;
-            this.radioButtonEnglish.TabStop = true;
-            this.radioButtonEnglish.Text = "Английский";
-            this.radioButtonEnglish.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.Location = new System.Drawing.Point(306, 48);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(144, 24);
-            this.checkBox2.TabIndex = 11;
-            this.checkBox2.Text = "Перестановки";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.Location = new System.Drawing.Point(305, 78);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(145, 24);
-            this.checkBox3.TabIndex = 12;
-            this.checkBox3.Text = "Гаммирование";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.Location = new System.Drawing.Point(305, 108);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(174, 47);
-            this.checkBox4.TabIndex = 13;
-            this.checkBox4.Text = "Аналитическое шифрование";
-            this.checkBox4.UseVisualStyleBackColor = true;
-            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.radioButtonEnglish);
-            this.Controls.Add(this.radioButtonRussian);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox2);
@@ -198,18 +126,6 @@ namespace EncodingApp
             this.ResumeLayout(false);
             this.PerformLayout();
         }
-
-        private System.Windows.Forms.CheckBox checkBox4;
-
-        private System.Windows.Forms.CheckBox checkBox3;
-
-        private System.Windows.Forms.CheckBox checkBox2;
-
-        private System.Windows.Forms.RadioButton radioButtonEnglish;
-
-        private System.Windows.Forms.RadioButton radioButtonRussian;
-
-        private System.Windows.Forms.CheckBox checkBox1;
 
         private System.Windows.Forms.Button button2;
 
--- EncodingApp/logic/VigenereEncoder.cs
+++ EncodingApp/logic/VigenereEncoder.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EncodingApp.logic
 {
-    class VigenereEncoder : IEncoder
+    class VigenereEncoder 
     {
 
         private string key;
