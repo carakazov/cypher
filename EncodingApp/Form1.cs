1181392c49054eaec795a4006a98fd24db5e541b
carakazov <carakazov@yandex.ru>
2021-05-11 22:35:07 +0300

    Гаммы

--- EncodingApp/Form1.cs
+++ EncodingApp/Form1.cs
@@ -14,7 +14,7 @@ namespace EncodingApp
     public partial class Form1 : Form
     {
         private string alphabet;
-        private IEncoder encoder;
+        private GammaEncoder encoder;
         public Form1()
         {
             InitializeComponent();
@@ -22,45 +22,13 @@ namespace EncodingApp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            encoder = new GammaEncoder();
             richTextBox2.Text = encoder.Encode(richTextBox1.Text);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             richTextBox1.Text = encoder.Decode(richTextBox2.Text);
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            DefineAlphabet();
-            encoder = new VigenereEncoder(textBox1.Text, alphabet);
-        }
-        
-        private void DefineAlphabet()
-        {
-            if (radioButtonEnglish.Checked)
-            {
-                alphabet = Utils.LatinAlphabet;
-            }
-            else
-            {
-                alphabet = Utils.CyrillicAlphabet;
-            }
-        }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-            encoder = new ShuffleEncoder(textBox1.Text);
-        }
-
-        private void checkBox3_CheckedChanged(object sender, EventArgs e)
-        {
-            encoder = new GammaEncoder();
-        }
-
-        private void checkBox4_CheckedChanged(object sender, EventArgs e)
-        {
-            encoder = new MatrixEncoder();
         }
     }
 }
--- EncodingApp/logic/GammaEncoder.cs
+++ EncodingApp/logic/GammaEncoder.cs
@@ -5,9 +5,41 @@ using System.Text;
 
 namespace EncodingApp.logic
 {
-    public class GammaEncoder : IEncoder
+    public class GammaEncoder
     {
-        private static Dictionary<char, string> gammaMatrix = Utils.GammaMatrix;
+        private static Dictionary<char, string> gammaMatrix = new Dictionary<char, string>()
+        {
+            {'а',"000001"},
+            {'б',"001001"},
+            {'в',"001010"},
+            {'г',"001011"},
+            {'д',"001100"},
+            {'е',"000010"},
+            {'ж',"001101"},
+            {'з',"001110"},
+            {'и',"000011"},
+            {'к',"001111"},
+            {'л',"010000"},
+            {'м',"010001"},
+            {'н',"010010"},
+            {'о',"000100"},
+            {'п',"010011"},
+            {'р',"010100"},
+            {'с',"010101"},
+            {'т',"010110"},
+            {'у',"000101"},
+            {'ф',"010111"},
+            {'х',"011000"},
+            {'ц',"011001"},
+            {'ч',"011010"},
+            {'ш',"011011"},
+            {'щ',"011100"},
+            {'ы',"011101"},
+            {'ь',"011110"},
+            {'э',"000110"},
+            {'ю',"000111"},
+            {'я',"001000"},
+        };
         private int[] gammaSequence = new[] {2, 3, 10, 4, 1, 5, 6, 7, 8, 11, 15, 14, 12, 13, 9, 0};
         public string Encode(string plainText)
         {
